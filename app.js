a47ba398dcc6fcb864ecc0bec0a7241a7c3101c8
hardikphp <hardik.mca08@gmail.com>
2019-09-06 13:06:40 +0530

    app js

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const router = require("./router/student.router");
+var mongoose = require("mongoose");
+require("dotenv").config();
+
+const sgMail = require("@sendgrid/mail");
+
+const app = express();
+mongoose.connect("mongodb://localhost:27017/Student_APP", {
+    useNewUrlParser: true
+});
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection time error"));
+
+let port = 5000;
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Request-With, Content-Type, Accept"
+    );
+    next();
+});
+app.use("/", router);
+
+app.listen(port, () => {
+    console.log("server is running at ", port);
+});
